1abfb616ade3c16c1dedd9249f29912de050dc21
chukusuccess <successchuku5@gmail.com>
2023-05-07 15:35:51 +0100

    proxie landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,186 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex w-full flex-col items-center justify-start py-6 sm:py-12">
+      <section className="flex w-full h-screen flex-col items-center justify-start bg1">
+        <Image src="/bg1.png" alt="background" fill />
+        <div className="w-full flex flex-row items-center justify-between px-6 sm:px-12">
+          <div>
+            <Image src="/proxie.svg" alt="proxie logo" width={50} height={50} />
+          </div>
+          <div>
+            <Image src="/menu.svg" alt="hamburger" width={30} height={30} />
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className="flex flex-col w-full h-full gap-10 px-6 sm:px-12">
+          <div>
+            <Image src="/hero.svg" alt="background" width={1000} height={50} />
+          </div>
+          <div>
+            <p className="text-left w-full text-2xl font-light text-[#B4ADAD]">
+              Be a part of the proxie users and get full control of your home or
+              offices with our proxie socket.
+            </p>
+          </div>
+        </div>
+        <div className="w-full flex flex-row items-center justify-end">
+          <Image src="/hand.svg" alt="smart device" width={300} height={100} />
+        </div>
+        <div className="absolute -bottom-5 flex flex-row items-center justify-between w-[90%]">
+          <p className="text-[#B4ADAD] italic ">
+            Get Connected <br />
+            To Your Devices
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <button className="bg-transparent border-2 border-red-600 rounded-lg px-2 py-1 text-red-600">
+            install Proxie
+          </button>
+        </div>
+      </section>
+      <section className="flex w-full flex-col items-center justify-start bg-[#262626] pt-6 sm:pt-12 px-6 sm:px-12">
+        <br />
+        <div className="w-full">
+          <Image src="/hero2.svg" alt="background" width={1000} height={50} />
+        </div>
+        <br />
+        <br />
+        <div className="w-full flex flex-row items-start justify-between relative">
+          <div className="text-[#B4ADAD] absolute">
+            <p>• Equipment/Appliance Safety</p>
+            <p>• Better Power Management</p>
+            <p>• Timer Enabled</p>
+            <p>• Shock Prevention</p>
+          </div>
+          <div className="w-full flex flex-row items-center justify-end">
+            <Image
+              src="/woman.svg"
+              alt="smart woman"
+              width={250}
+              height={100}
+            />
+          </div>
+        </div>
+      </section>
+      <section className="flex w-full flex-col items-center justify-start bg-[#262626] bg2">
+        <Image src="/bg2.png" alt="background" width={1000} height={100} />
+        <div className="flex flex-col w-full h-full gap-10 px-6 sm:px-12 absolute pt-10">
+          <div>
+            <Image src="/hero3.svg" alt="background" width={250} height={50} />
+          </div>
+        </div>
+      </section>
+      <section className="flex w-full flex-col items-center justify-start bg-[#262626] px-6 sm:px-12">
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+          <div class="grid gap-4">
+            <div>
+              <br />
+              <br />
+              <br />
+              <Image
+                width={200}
+                height={100}
+                class="h-auto max-w-full rounded-lg"
+                src="/leftfirst.png"
+                alt="proxie masonry"
+              />
+            </div>
+            <div>
+              <Image
+                width={200}
+                height={100}
+                class="h-auto max-w-full rounded-lg"
+                src="/leftmid.png"
+                alt="proxie masonry"
+              />
+            </div>
+            <div>
+              <Image
+                width={200}
+                height={100}
+                class="h-auto max-w-full rounded-lg"
+                src="/leftlast.png"
+                alt="proxie masonry"
+              />
+            </div>
+          </div>
+          <div class="grid gap-4">
+            <div>
+              <Image
+                width={200}
+                height={100}
+                class="h-auto max-w-full rounded-lg"
+                src="/rightfirst.png"
+                alt="proxie masonry"
+              />
+            </div>
+            <div>
+              <Image
+                width={200}
+                height={100}
+                class="h-auto max-w-full rounded-lg"
+                src="/rightlast.png"
+                alt="proxie masonry"
+              />
+            </div>
+            <div className="w-full h-[150px] text-[#34A853] text-center">
+              <p>Checkout All Our Products</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="flex w-full flex-col items-center justify-start bg-[#262626] px-6 sm:px-12">
+        <br />
+        <br />
+        <br />
+        <div>
+          <Image
+            src="/youtube.png"
+            alt="proxie video"
+            width={1000}
+            height={50}
+          />
+        </div>
+      </section>
+      <section className="flex w-full flex-col items-center justify-start bg-[#262626] px-6 sm:px-12">
+        <br />
+        <br />
+        <br />
+        <div className="w-full flex flex-row items-start justify-start">
+          <div className="flex flex-col gap-5 items-start justify-start w-1/2 text-[#B4ADAD]">
+            <p>
+              <b>Contact</b>
+            </p>
+            <p>Loram Ipsum</p>
+            <p>Dolor sit</p>
+            <p>Consecteur</p>
+            <p>Enim Venenatis</p>
+            <p>Eget Eros</p>
+          </div>
+          <div className="flex flex-col gap-5 items-start justify-start w-1/2 text-[#B4ADAD]">
+            <p>
+              <b>Faq</b>
+            </p>
+            <p>Loram Ipsum</p>
+            <p>Dolor sit</p>
+            <p>Consecteur</p>
+            <p>Enim Venenatis</p>
+          </div>
+        </div>
+        <br />
+        <br />
+      </section>
+      <section className="flex w-full flex-col items-center justify-start bg-[#B4ADAD] py-4 px-6 sm:px-12">
+        <br />
+        <div className="flex flex-row font-light text-black w-full items-center justify-between">
+          <p>Subscribe To Our Newsletter</p>
+          <input className="text-black font-light border-2 rounded-lg border-[#424141] w-1/4" />
+        </div>
+        <br />
+      </section>
     </main>
-  )
+  );
 }
